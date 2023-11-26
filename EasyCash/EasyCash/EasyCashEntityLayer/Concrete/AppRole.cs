208203150f766c617042699c3af52fddffbc1363
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-11-26 16:13:02 +0300

    before migrations

--- EasyCash/EasyCash/EasyCashEntityLayer/Concrete/AppRole.cs
+++ EasyCash/EasyCash/EasyCashEntityLayer/Concrete/AppRole.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace EasyCashEntityLayer.Concrete
 {
-    internal class AppRole
+    public class AppRole : IdentityRole<int>
     {
     }
 }
--- EasyCash/EasyCash/EasyCashEntityLayer/Concrete/AppUser.cs
+++ EasyCash/EasyCash/EasyCashEntityLayer/Concrete/AppUser.cs
@@ -1,13 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyCashEntityLayer.Concrete
 {
-    public class AppUser:IdentityUser<int>
+    public class AppUser : IdentityUser<int>
     {
         public string Name { get; set; }
         public string Surname { get; set; }
