f1ea9e571955f9697ee5c6e4088b6dc7207a6861
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-11-26 15:48:59 +0300

    REFERENCES

--- EasyCash/EasyCash/EasyCashEntityLayer/Concrete/CustomerAccountProcess.cs
+++ EasyCash/EasyCash/EasyCashEntityLayer/Concrete/CustomerAccountProcess.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EasyCashEntityLayer.Concrete
+﻿namespace EasyCashEntityLayer.Concrete
 {
-    internal class CustomerAccountProcess
+    public class CustomerAccountProcess
     {
+        public int CustomerAccountProcessId { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
