11079ccff6609165d868e70db1b3c55146a5038d
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-11-26 16:12:16 +0300

    IdentityDbContext<AppUser, AppRole, int>

--- EasyCash/EasyCash/EasyCashIdentityDataAccessLayer/Concrete/Context.cs
+++ EasyCash/EasyCash/EasyCashIdentityDataAccessLayer/Concrete/Context.cs
@@ -1,20 +1,16 @@
 ﻿using EasyCashEntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyCashIdentityDataAccessLayer.Concrete
 {
-    public class Context:IdentityDbContext
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-RDAT0MB\\SQLEXPRESS;database=EasyCashIdentity;integrated security=true;");
         }
+
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
     }
