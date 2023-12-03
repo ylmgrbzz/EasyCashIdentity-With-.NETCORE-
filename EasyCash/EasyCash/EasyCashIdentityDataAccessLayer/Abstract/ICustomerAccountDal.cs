76100da28567c44eb12438e05866a7c59e7d6ff4
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-12-03 17:44:16 +0300

    ICustomerAccountProcessDal

---
+++ EasyCash/EasyCash/EasyCashIdentityDataAccessLayer/Abstract/ICustomerAccountDal.cs
@@ -0,0 +1,13 @@
+﻿using EasyCashEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityDataAccessLayer.Abstract
+{
+    public interface ICustomerAccountDal:IGenericDal<CustomerAccount>
+    {
+    }
+}
