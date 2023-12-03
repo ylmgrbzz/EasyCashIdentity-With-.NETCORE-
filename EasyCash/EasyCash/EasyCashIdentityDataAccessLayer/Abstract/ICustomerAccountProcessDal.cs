fd2a8c2047afb6fa5c3313934d22d85c015f29de
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-12-03 18:10:27 +0300

    CustomerAccountProcessManager

--- EasyCash/EasyCash/EasyCashIdentityDataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
+++ EasyCash/EasyCash/EasyCashIdentityDataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyCashEntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityDataAccessLayer.Abstract
 {
-    internal interface ICustomerAccountProcessDal
+    public interface ICustomerAccountProcessDal:IGenericDal<CustomerAccountProcess>
     {
     }
 }
