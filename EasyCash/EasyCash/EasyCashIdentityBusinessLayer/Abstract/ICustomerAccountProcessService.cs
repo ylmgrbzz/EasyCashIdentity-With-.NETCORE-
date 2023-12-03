76846cb47d5b05ac5433e91353567a91a689153f
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-12-03 18:04:35 +0300

    CustomerAccountManager

---
+++ EasyCash/EasyCash/EasyCashIdentityBusinessLayer/Abstract/ICustomerAccountProcessService.cs
@@ -0,0 +1,13 @@
+﻿using EasyCashEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityBusinessLayer.Abstract
+{
+    internal interface ICustomerAccountProcessService:IGenericService<CustomerAccountProcess>
+    {
+    }
+}
