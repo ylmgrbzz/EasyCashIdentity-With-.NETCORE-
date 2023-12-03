e32397a71e6cf4dcbc0b4a4d2b21197324c4e453
Yalım Gürbüz <ygurbuz@lstyazilim.com>
2023-12-03 17:29:32 +0300

    AppUserRegisterValidator

---
+++ EasyCash/EasyCash/EasyCashIdentityBusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,31 @@
+﻿using EasyCashEntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyCashIdentityDtoLayer.Dtos.AppUserDtos;
+
+namespace EasyCashIdentityBusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+                RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre boş geçilemez");
+            RuleFor(x => x.Password).Equal(x => x.ConfirmPassword).WithMessage("Şifreler uyuşmuyor");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email boş geçilemez");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Geçerli bir email adresi giriniz");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyisim boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("İsim en az 3 karakter olmalıdır");
+            RuleFor(x => x.Surname).MinimumLength(3).WithMessage("Soyisim en az 3 karakter olmalıdır");
+            RuleFor(x => x.Password).MinimumLength(5).WithMessage("Şifre en az 5 karakter olmalıdır");
+            RuleFor(x => x.UserName).MinimumLength(5).WithMessage("Kullanıcı adı en az 5 karakter olmalıdır");
+
+
+        }
+    }
+}
--- EasyCash/EasyCash/EasyCashIdentityDtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
+++ EasyCash/EasyCash/EasyCashIdentityDtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
